90a0b9d74472d342f610185e4914bff44e8c99e7
stein-fabricio <acfastdc@hotmail.com>
2024-08-28 20:33:30 -0300

    First draft

---
+++ WeatherApp-SwiftUI/WeatherApp-SwiftUI/WeatherApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp_SwiftUIApp.swift
+//  WeatherApp-SwiftUI
+//
+//  Created by Stein on 28/08/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
